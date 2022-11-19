b0fde968c7a1f9dc08b03163ded5a8e0beff8571
Cupskrrt <khanalfarizzi@hotmail.com>
2022-11-19 22:45:09 +0700

    Still trying to get pagination done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,30 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useApi from '../hooks/useApi';
 
 import { Link } from 'react-router-dom';
 
 const PokemonCard = () => {
-  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/');
+  const url = 'https://pokeapi.co/api/v2/pokemon'
 
   const pokemonList = useApi(url);
 
- 
+  const handleClickNext = (e) => {
+    e.preventDefault()
+    const next = pokemonList ? pokemonList.next : ''
+    console.log(next)
+  }
+
+  const handleClickPrev = (e) => {
+    e.preventDefault()
+    const prev = pokemonList ? pokemonList.prev : ''
+    console.log(prev)
+  }
+
+  
+
 
   return (
     <>
-      {pokemonList ? (
-        pokemonList.results.map((item, i) => {
-          return (
-              <Link to={`/detail/${item.name}`}
-                key={i}
-                className="w-[8rem] h-[2.5rem] border-2">
-                {item.name}
-              </Link>
-          );
-        })
-      ) : (
-        <p>No Data</p>
-      )}
+      <div className='grid grid-cols-2 justify-items-center content-center'>
+        {pokemonList ? (
+          pokemonList.results.map((item, i) => {
+            return (
+                <Link to={`/detail/${item.name}`}
+                  key={i}
+                  className="flex items-center justify-center w-[8rem] h-[2.5rem] border-2">
+                  {item.name}
+                </Link>
+            );
+          })
+        ) : (
+          <p>No Data</p>
+        )}
+      </div>
+      <div className='flex items-center justify-between p-6'>
+          <button onClick={handleClickPrev}> PREV </button>
+          <button onClick={handleClickNext}> NEXT </button>
+        </div>
     </>
   );
 };
--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -1,17 +1,51 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import useApi from '../hooks/useApi';
 
 const PokemonDetail = () => {
   const { pokemon } = useParams();
   const pokemonDetail = useApi(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-  const img = pokemonDetail ? pokemonDetail.sprites.front_default : '';
+  const type = pokemonDetail
+    ? pokemonDetail.types.map((item) => item.type)
+    : '';
+
+  const base_stat = pokemonDetail
+    ? pokemonDetail.stats.map((item) => item.base_stat)
+    : '';
+  const stat_name = pokemonDetail
+    ? pokemonDetail.stats.map((item) => item.stat).map((data) => data.name)
+    : '';
+
   console.log(pokemonDetail);
+
   return (
     <>
-      <img src={img} alt={pokemon} />
-      <h1>{pokemon}</h1>
+      <div className="flex justify-around">
+        <img
+          src={pokemonDetail ? pokemonDetail.sprites.front_default : ''}
+          alt={pokemon}
+          className="w-[10rem]"
+        />
+        <img
+          src={pokemonDetail ? pokemonDetail.sprites.back_default : ''}
+          alt={pokemon}
+          className="w-[10rem]"
+        />
+      </div>
+      <h1>{pokemonDetail ? pokemonDetail.name.toUpperCase() : ''}</h1>
+      <p>id: {pokemonDetail ? pokemonDetail.id : ''}</p>
+      <div className="flex gap-3">
+        {type
+          ? type.map((item, i) => {
+              return <h2 key={i}>{item.name}</h2>;
+            })
+          : ''}
+      </div>
+      {/* BIKIN CHARTNYA MASI BINGUNG */}
+      <h2>Height: {pokemonDetail ? pokemonDetail.height : ''}</h2>
+      <h2>Weight: {pokemonDetail ? pokemonDetail.weight : ''}</h2>
+      <Link to="/"> BACK </Link>
     </>
   );
 };
